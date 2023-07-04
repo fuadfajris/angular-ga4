b99537fd942cc2139012a677a61196f691586343
fuad fajri soba <fuadfajrisoba@gmail.com>
2023-07-04 07:50:54 +0700

    test ga4 tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,10 +29,18 @@ export class AppComponent implements OnInit {
 
   testga1() {
     console.log("test1")
+    gtag('event', 'screen_view', {
+      'app_name': 'myAppName1',
+      'screen_name': 'Test Home 1'
+    });
   }
 
   testga2() {
     console.log("test2")
+    gtag('event', 'screen_view', {
+      'app_name': 'myAppName2',
+      'screen_name': 'Test Home 2'
+    });
   }
 
 }
