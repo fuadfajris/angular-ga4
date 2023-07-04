f9ee3434bb1592bfd2c5cb51d2d99ced533cd3e0
fuad fajri soba <fuadfajrisoba@gmail.com>
2023-07-04 08:25:46 +0700

    test ga4 tag(2)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
+import { Angulartics2Module } from 'angulartics2';
+import { Angulartics2GoogleAnalytics } from 'angulartics2';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    Angulartics2Module.forRoot(),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Angulartics2GoogleAnalytics } from 'angulartics2';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +12,16 @@ export class AppComponent implements OnInit {
   title = 'my-app';
 
   constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private titleService: Title
+    public angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics,
+
   ) {
+    angulartics2GoogleAnalytics.startTracking();
     // gtag('event', 'page_view', {page_path: 'https://angular-ga4.netlify.app/'})
-    gtag('event', 'screen_view', {
-      'app_name': 'myAppName',
-      'screen_name': 'Home'
-    });
+
+    // gtag('event', 'screen_view', {
+    //   'app_name': 'myAppName',
+    //   'screen_name': 'Home'
+    // });
   }
 
   ngOnInit() {
@@ -29,18 +30,25 @@ export class AppComponent implements OnInit {
 
   testga1() {
     console.log("test1")
-    gtag('event', 'screen_view', {
-      'app_name': 'myAppName1',
-      'screen_name': 'Test Home 1'
-    });
+    this.angulartics2GoogleAnalytics.eventTrack(
+      'event',{ 'app_name': 'myAppName1', 'screen_name': 'Test Home 1' }
+    )
+    // gtag('event', 'screen_view', {
+    //   'app_name': 'myAppName1',
+    //   'screen_name': 'Test Home 1'
+    // });
   }
 
   testga2() {
     console.log("test2")
-    gtag('event', 'screen_view', {
-      'app_name': 'myAppName2',
-      'screen_name': 'Test Home 2'
-    });
+    this.angulartics2GoogleAnalytics.eventTrack(
+      'event',{ 'app_name': 'myAppName2', 'screen_name': 'Test Home 2' }
+    )
+
+    // gtag('event', 'screen_view', {
+    //   'app_name': 'myAppName2',
+    //   'screen_name': 'Test Home 2'
+    // });
   }
 
 }
