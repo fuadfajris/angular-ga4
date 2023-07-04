0fa73a182a2d28603659b42487a498b50bfc6609
fuad fajri soba <fuadfajrisoba@gmail.com>
2023-07-04 07:44:11 +0700

    update ga tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) {
+    // gtag('event', 'page_view', {page_path: 'https://angular-ga4.netlify.app/'})
+    gtag('event', 'screen_view', {
+      'app_name': 'myAppName',
+      'screen_name': 'Home'
+    });
+  }
+
+  ngOnInit() {
+    window.scrollTo(0, 0);
+  }
+
+  testga1() {
+    console.log("test1")
+  }
+
+  testga2() {
+    console.log("test2")
+  }
+
 }
