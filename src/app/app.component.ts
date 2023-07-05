4e0ca92b0f3d568df807bb809a121ef599065b0b
fuad fajri soba <fuadfajrisoba@gmail.com>
2023-07-05 16:00:16 +0700

    edit data view(2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,6 +66,34 @@ export class AppComponent implements OnInit {
       'app_name': 'myAppName2',
       'screen_name': 'Test Home 2'
     });
+
+    gtag("event", "view_life_declaration", {
+      item_list_id: "related_products",
+      item_list_name: "Related products",
+      items: [
+        {
+          item_id: "SKU_12345",
+          item_name: "Stan and Friends Tee",
+          affiliation: "Google Merchandise Store",
+          coupon: "SUMMER_FUN",
+          discount: 2.22,
+          index: 0,
+          item_brand: "Google",
+          item_category: "Apparel",
+          item_category2: "Adult",
+          item_category3: "Shirts",
+          item_category4: "Crew",
+          item_category5: "Short sleeve",
+          item_list_id: "related_products",
+          item_list_name: "Related Products",
+          item_variant: "green",
+          location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
+          price: 9.99,
+          quantity: 1
+        }
+      ]
+    });
+  
   }
 
 }
